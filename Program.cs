001b7b41d9a1a5bb780bbd4565a79062d1df770f
Renaulte <95443035+Renaulte@users.noreply.github.com>
2021-12-03 11:50:54 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,42 +8,56 @@ namespace Dice_Game
         {
             #region variable declarations
             string userInput;
+            int timesToRun;
             string userColor;
-            string diceColor;
-            int userNum;
+            string diceColor = null;
+            int diceSides;
             int userColorNum = 1;
-            bool loopLogicSides = false;
             bool loopLogicColor = false;
+            bool isNumValid = false;
+            bool isStrValid = false;
+            bool isNumInRange = false;
             #endregion
 
             #region User Input Sides
-            //first line of code to ask user to input number of sides on the dice
-            while (loopLogicSides == false)
-            {
-                Console.WriteLine("Welcome! Let's set up your dice. How many sides do you want your dice to have?\nEnter a number between 2 to 20:");
-                string userNumStr = Console.ReadLine();
 
-                if (Int32.TryParse(userNumStr, out userNum))
+
+            Console.WriteLine("Welcome! Let's set up your dice. How many sides do you want your dice to have?\nEnter a number between 2 to 20:");
+            do
+            {
+                string diceSidesStr = Console.ReadLine();
+
+                isNumValid = Int32.TryParse(diceSidesStr, out diceSides);
+                if (isNumValid == true)
                 {
-                    Console.WriteLine($"Number of sides set to {userNum}.");
-                    loopLogicSides = true;
-                    
+                    if (diceSides > 1 && diceSides < 21)
+                    {
+                        Console.WriteLine($"Number of sides set to {diceSides}.");
+                        isNumInRange = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input must be between 2 and 20.\n");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"Could not read data. Try again.");
+                    Console.WriteLine($"Invalid input, please try again.\n");
                 }
-            }
+            } while (isNumValid == false || isNumInRange == false);
+
 
             #endregion
 
             #region User Input Color
             //code to ask user to select a dice color
-            while (loopLogicColor == false)
+            Console.WriteLine("Now let's pick a dice color. Input a number to select your color:");
+
+            do
             {
 
-                Console.WriteLine("Now let's pick a dice color. Input a number to select your color:");
-                Console.WriteLine("1. Blue\n2. Red\n3. Clear");
+
+                Console.WriteLine("1. Red\n2. Green\n3. Blue\n4. Transparent");
                 userColor = Console.ReadLine();
 
                 if (Int32.TryParse(userColor, out userColorNum))
@@ -52,19 +66,25 @@ namespace Dice_Game
                     {
                         case 1:
                             userColorNum = 1;
-                            diceColor = "Blue";
+                            diceColor = "Red";
                             loopLogicColor = true;
                             Console.WriteLine($"Dice color set to {diceColor}.");
                             break;
                         case 2:
                             userColorNum = 2;
-                            diceColor = "Red";
+                            diceColor = "Green";
                             loopLogicColor = true;
                             Console.WriteLine($"Dice color set to {diceColor}.");
                             break;
                         case 3:
                             userColorNum = 3;
-                            diceColor = "Clear";
+                            diceColor = "Blue";
+                            loopLogicColor = true;
+                            Console.WriteLine($"Dice color set to {diceColor}.");
+                            break;
+                        case 4:
+                            userColorNum = 4;
+                            diceColor = "Transparent";
                             loopLogicColor = true;
                             Console.WriteLine($"Dice color set to {diceColor}.");
                             break;
@@ -75,63 +95,78 @@ namespace Dice_Game
 
                 }
             }
-
+            while (loopLogicColor == false);
             #endregion
 
-            Dice dice = new Dice(userNum, "red");
+            #region Main Logic
 
+            Dice userDice = new(diceSides, diceColor);
+            bool checkForOne = false;
 
-            Console.WriteLine("Input a number to roll that many D20s. Roll a nat 20 to win!");
-            userInput = Console.ReadLine();
+            do 
+            {
+                Console.WriteLine($"Input a number to roll that many {userDice.Color} D{userDice.NumSides}. Get a max roll of {userDice.NumSides} to win!");
+                userInput = Console.ReadLine();
+                Int32.TryParse(userInput, out timesToRun);
 
-            //if(userInput == "Nonstop!")
-            //{
-            //    int checkForOne = 0;
-            //    int numTimesRun = 0;
-            //    while (checkForOne != 1)
-            //    {
-            //        numTimesRun++;
-            //        checkForOne = DiceGameMethod();
-            //        if (checkForOne == 1)
-            //        {
-            //            Console.WriteLine($"It took you {numTimesRun} times to get a nat 20 on the first try!");
-            //            break;
-            //        }
+                if (userInput == "Nonstop!")
+                {
                     
-                    
-            //    }
-            //}
-            //else
-            //{
-            //    DiceGameMethod();
-            //}
+                    int numTimesRun = 0;
+                    while (checkForOne != true)
+                    {
+                        numTimesRun++;
+                        checkForOne = DiceGameMethod(userDice.NumSides, timesToRun, userDice.Color);
+                        if (checkForOne == true)
+                        {
+                            Console.WriteLine($"It took you {numTimesRun} times to get a max roll on the first try!");
+                            break;
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    checkForOne = DiceGameMethod(userDice.NumSides, timesToRun, userDice.Color);
+                }
+            } while (checkForOne == false);
             Console.ReadLine();
         }
+        #endregion
 
-        public static int DiceGameMethod()
+        #region Dice Game Method
+        public static bool DiceGameMethod(int diceSides, int timesToRun, string diceColor)
         {
             string userConfirm;
             bool userVar;
             int nextRandomNumber = 0;
             int timesRun = 0;
-            Random random = new Random();
+            int timesWon = 0;
+            bool winCon = false;
+            Random random = new();
 
             do
             {
-                while (nextRandomNumber != 20)
+                while (winCon == false)
                 {
-                    nextRandomNumber = random.Next(1, 21);
-                    Console.WriteLine(nextRandomNumber);
+                    nextRandomNumber = random.Next(1, diceSides + 1);
+                    Console.WriteLine($"Roll #{timesRun}: Result: {nextRandomNumber}");
                     timesRun++;
-                    if (timesRun == 1 && nextRandomNumber == 20)
+                    if (timesRun == 1 && nextRandomNumber == diceSides)
                     {
-                        Console.WriteLine("You got a nat 20! It only took one try!");
-                        return 1;
+                        Console.WriteLine($"You got a max roll with the {diceColor} die on the first try!");
+                        timesWon++;
                     }
-                    else if (nextRandomNumber == 20)
+                    else if (nextRandomNumber == diceSides)
                     {
-                        Console.WriteLine($"You got a nat 20! It only took {timesRun} rolls.");
-                        return 0;
+                        Console.WriteLine($"Max roll with the {diceColor} die! This is roll #{timesRun}.");
+                        timesWon++;
+                    }
+                    if(timesRun == timesToRun)
+                    {
+                        Console.WriteLine($"You won {timesWon} times in {timesToRun} rolls.");
+                        winCon = true;
                     }
 
                 }
@@ -142,17 +177,16 @@ namespace Dice_Game
                     userVar = true;
                     nextRandomNumber = 0;
                     timesRun = 0;
-                    return 0;
+                    return false;
                 }
                 else
                 {
                     userVar = false;
+                    return true;
                 }
-                return 0;
-
             } while (userVar == true);
         }
     }
-        
-    
+    #endregion
+
 }
